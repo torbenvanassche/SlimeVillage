533ef5f08f937866b4de6fd2e159109465b79f4b
Torben Van Assche <torbenvanassche.anime@gmail.com>
2023-10-14 14:07:50 +0200

    fixed camera not retaining position after locking and unlocking

--- scripts/Utils/Camera3D.gd
+++ scripts/Utils/Camera3D.gd
@@ -11,6 +11,8 @@ var mouse_sensitivity := 0.001
 var pitch_input: float = 0
 var twist_input: float = 0
 
+var mouse_position:Vector2
+
 func _ready():
 	$TwistPivot/PitchPivot/Camera3D.position.z = distance
 	
@@ -24,9 +26,11 @@ func _process(_delta):
 	
 func _input(_event):
 	if Input.is_action_just_pressed("rotate_camera"):
+		mouse_position = get_viewport().get_mouse_position()
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	elif Input.is_action_just_released("rotate_camera"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		get_viewport().warp_mouse(mouse_position)
 		
 	var dist = %Camera3D.position.distance_to(Global.player_instance.position)
 	if Input.is_action_just_pressed("scroll_wheel_up") and dist > min_distance:
