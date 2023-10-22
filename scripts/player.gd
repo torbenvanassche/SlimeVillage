f8338d5204b92b06973e671a1a1c627cda30c014
Torben Van Assche <torbenvanassche.anime@gmail.com>
2023-10-23 00:21:58 +0200

    working on character facing rotation

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,16 +5,19 @@ var timer: Timer = null
 var nav_index = 0
 var progress: float = 0
 
-@export var t:Node3D
-
-@export var current_tile: Tile = null
+var current_tile: Tile = null
 var destination_tile: Tile = null
 var inventory: Inventory = Inventory.new()
+
+@export var move_delay: float = 0.25
+@export var rotation_time: float = 0.1
 
 func move():
 	nav_index = 0
 	if timer.is_stopped():
 		timer.start()
+		
+	look_to_direction()
 		
 func set_position_to_current_tile(tile: Tile = current_tile):
 	current_tile = tile
@@ -24,9 +27,6 @@ func _process(_delta):
 	if Global.scene_manager.active_scene.pathfinder.current_nav.size() != 0:
 		progress = 1 - (timer.time_left / timer.wait_time)
 		position = current_tile.surface_point.slerp(Global.scene_manager.active_scene.pathfinder.current_nav[nav_index].surface_point, progress)
-		
-		var lookat_target = -Global.scene_manager.active_scene.pathfinder.current_nav[nav_index].surface_point
-		$mesh.look_at(Vector3(lookat_target.x, self.global_position.y, lookat_target.z), Vector3.UP)
 
 func _ready():
 	Global.player_instance = self
@@ -34,7 +34,7 @@ func _ready():
 	timer = Timer.new()
 	add_child(timer)
 		
-	timer.wait_time = 0.25
+	timer.wait_time = move_delay
 	timer.one_shot = false
 	timer.timeout.connect(_on_time)
 	
@@ -50,6 +50,18 @@ func find_location() -> Tile:
 		return result.collider.get_parent() as Tile
 		
 	return null
+	
+func look_to_direction():
+	var tween := create_tween()
+	tween.tween_method(_tween_rotation, 0.0, 1.0, rotation_time)
+
+func _tween_rotation(p):
+	var lookat_target = Global.scene_manager.active_scene.pathfinder.current_nav[nav_index].surface_point
+	var lookat_transform = $mesh.global_transform.looking_at(lookat_target, Vector3.UP)
+	var initial_basis = $mesh.global_transform.basis
+	
+	$mesh.global_transform.basis = initial_basis.slerp(lookat_transform.basis, p)
+	$mesh.rotate_y(deg_to_rad(180))
 
 func _on_time():
 	if Global.scene_manager.active_scene.pathfinder.current_nav.size() == 0:
