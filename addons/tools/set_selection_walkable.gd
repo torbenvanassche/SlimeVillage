de804de5079c894ced7b993c3b4bddb238ddec26
Torben Van Assche <torbenvanassche.anime@gmail.com>
2024-01-10 00:36:14 +0100

    added walkable tiles

--- addons/tools/set_selection_walkable.gd
+++ addons/tools/set_selection_walkable.gd
@@ -6,4 +6,6 @@ var packed_scene: PackedScene = preload("res://scenes/components/tile.tscn")
 func _run():
 	var selected = get_editor_interface().get_selection().get_selected_nodes();
 	for node in selected:
-		node.add_child(packed_scene.instantiate())
+		var instance = packed_scene.instantiate();
+		node.add_child(instance)
+		instance.owner = get_scene()
