3c25143354930de8b674000fecc4332fd58c0275
Torben Van Assche <torbenvanassche.anime@gmail.com>
2024-02-28 16:10:50 +0100

    added market generator signal

--- scripts/mechanics/UI/market_ui_in_world.gd
+++ scripts/mechanics/UI/market_ui_in_world.gd
@@ -1,6 +1,14 @@
 extends Node
 
-@export var _poster_packed: Array[PackedScene]
+@export var _poster_packed: Array[PackedScene] = [];
+@export var market_generator: MarketGenerator;
 
 func get_poster():
 	return _poster_packed.pick_random().instantiate();
+
+func _ready():
+	market_generator.item_spawned.connect(_spawn_item)
+	
+func _spawn_item(inv: Inventory):
+	print(inv)
+	pass
--- scripts/mechanics/market.gd
+++ scripts/mechanics/market.gd
@@ -1,3 +1,4 @@
+class_name MarketGenerator
 extends Node
 
 var spawn_timer: Timer
@@ -5,22 +6,22 @@ var spawn_timer: Timer
 var current_orders: Array[Inventory];
 @onready var available_items := ItemManager.get_by_property("available", true);
 
-@export var spawn_delay = 1;
 @export var max_orders = 1;
 @onready var market_window := Global.ui_root.get_subwindow("MARKET");
 @export var amount_per_spawn = 1;
+@export var wait_time = 1;
 
 signal item_spawned(inv: Inventory);
 
 func _ready():
 	spawn_timer = Timer.new()
-	spawn_timer.wait_time = 1
+	spawn_timer.wait_time = wait_time
 	spawn_timer.timeout.connect(_on_spawn)
 	add_child(spawn_timer)
 	spawn_timer.start()
 
 func _on_spawn():
-	var inv = Inventory.new();
+	var inv = Inventory.new(4, "Market");
 	for a in amount_per_spawn:
 		var item = _generate_item();
 		inv.add_item(item, 1);
