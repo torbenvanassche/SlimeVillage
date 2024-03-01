033a6865dcb357a4a4a9cfa7539db317b5f04dfc
Torben Van Assche <torbenvanassche.anime@gmail.com>
2024-03-01 13:59:53 +0100

    successfully fetching child collider of posters

--- scripts/mechanics/UI/market_ui_in_world.gd
+++ scripts/mechanics/UI/market_ui_in_world.gd
@@ -5,7 +5,8 @@ var _spawn_positions: Array[Node] = [];
 @export var market_generator: MarketGenerator;
 
 func get_poster():
-	return _poster_packed.pick_random().instantiate();
+	return _poster_packed[0].instantiate();
+	#return _poster_packed.pick_random().instantiate();
 
 func _ready():
 	market_generator.item_spawned.connect(_spawn_item);
@@ -16,8 +17,9 @@ func _spawn_item(inv: Inventory):
 	if filtered.size() == 0:
 		return;
 	
-	var spawned: Node3D = filtered.pick_random();
+	var spawner_position: Node3D = filtered.pick_random();
 	var poster = get_poster();
-	spawned.set_meta("in_use", true);
-	spawned.set_meta("item", inv);
-	spawned.add_child(poster);
+	spawner_position.set_meta("in_use", true);
+	spawner_position.set_meta("item", inv);
+	print(poster.get_child(0, true).get_child(0, true));
+	spawner_position.add_child(poster);
